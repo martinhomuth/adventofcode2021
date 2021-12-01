cbfe646db29f6b375975195bc2698944beaff4da
Martin Homuth <martin@followthestack.tech>
2021-12-01 13:35:42 +0100

    add first solution
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

---
+++ 1/depth-measurement.lisp
@@ -0,0 +1,52 @@
+;; As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
+
+;; For example, suppose you had the following report:
+
+;; 199
+;; 200
+;; 208
+;; 210
+;; 200
+;; 207
+;; 240
+;; 269
+;; 260
+;; 263
+;; This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
+
+;; The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
+
+;; To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
+
+;; 199 (N/A - no previous measurement)
+;; 200 (increased)
+;; 208 (increased)
+;; 210 (increased)
+;; 200 (decreased)
+;; 207 (increased)
+;; 240 (increased)
+;; 269 (increased)
+;; 260 (decreased)
+;; 263 (increased)
+;; In this example, there are 7 measurements that are larger than the previous measurement.
+
+;; How many measurements are larger than the previous measurement?
+
+(defun get-input (filename)
+  (with-open-file (stream filename)
+    (loop for line = (read-line stream nil)
+       while line
+       collect (parse-integer line))))
+
+(defun count-recursive (cons-list acc)
+  (if (> (list-length cons-list) 1)
+      (progn
+	(if (> (cadr cons-list) (car cons-list))
+	    (setf acc (1+ acc)))
+	(count-recursive (cdr cons-list) acc))
+      acc))
+
+(defun list-measurement-increases ()
+  (let ((numbers (get-input "input.txt"))
+	(acc 0))
+    (count-recursive numbers acc)))
